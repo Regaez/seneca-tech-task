b8e59538e37870c7dee221864309d4e7d9adc84f
Thomas Threadgold <tj.threadgold@gmail.com>
2021-11-06 23:29:21 +0100

    feat: add upsertSession query

--- src/queries/sessions.ts
+++ src/queries/sessions.ts
@@ -41,7 +41,52 @@ export const getSession = async (params: {
       return err("NOT_FOUND");
     }
 
+    logger.debug({ session });
+
     return ok(session);
+  } catch (e) {
+    logger.error(e);
+    return err("QUERY_FAILED");
+  }
+};
+
+export const upsertSession = async (params: {
+  sessionId: string;
+  courseId: string;
+  userId: string;
+  totalModulesStudied: number;
+  averageScore: number;
+  timeStudied: number;
+}): Promise<Result<true, "QUERY_FAILED">> => {
+  try {
+    await sql`
+      INSERT INTO sessions (
+        session_id,
+        course_id,
+        user_id,
+        total_modules_studied,
+        average_score,
+        time_studied
+      ) VALUES (
+        ${params.sessionId},
+        ${params.courseId},
+        ${params.userId},
+        ${params.totalModulesStudied},
+        ${params.averageScore},
+        ${params.timeStudied}
+      )
+      ON CONFLICT (session_id) DO UPDATE
+      SET (
+        total_modules_studied,
+        average_score,
+        time_studied
+      ) = (
+        ${params.totalModulesStudied},
+        ${params.averageScore},
+        ${params.timeStudied}
+      )
+    `;
+    return ok(true);
   } catch (e) {
     logger.error(e);
     return err("QUERY_FAILED");

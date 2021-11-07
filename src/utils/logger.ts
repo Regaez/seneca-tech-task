4691cfde23d44e8494ce5fc321a56ab200a658ec
Thomas Threadgold <tj.threadgold@gmail.com>
2021-11-07 07:44:56 +0100

    refactor: use pino-pretty transport

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,16 @@ import pino from "pino";
 import { NODE_ENV, LOG_LEVEL } from "../configs";
 
 export const logger = pino({
-  prettyPrint: NODE_ENV === "production" ? false : {
-    colorize: true,
-    ignore: "pid,hostname",
-    translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l"
-  },
+  // Prettify logging for non-production environments
+  ...(NODE_ENV !== "production" && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: "pid,hostname",
+        translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l"
+      }
+    }
+  }),
   level: LOG_LEVEL
 });

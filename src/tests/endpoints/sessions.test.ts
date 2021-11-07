5f7d19955288e4445d0e686ca826776427d77358
Thomas Threadgold <tj.threadgold@gmail.com>
2021-11-07 15:19:48 +0100

    tests: cover POST sessions endpoint

--- src/tests/endpoints/sessions.test.ts
+++ src/tests/endpoints/sessions.test.ts
@@ -184,4 +184,306 @@ describe("Endpoint: sessions", () => {
 
   });
 
+  describe("POST /courses/:course_id", () => {
+
+    describe("should return status 400 when", () => {
+      it("no `x-user-id` header is given", async () => {
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `x-user-id` header is not uuid format", async () => {
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": "invalid",
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("no `course_id` param is given", async () => {
+        const course_id = "";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `course_id` param is not uuid format", async () => {
+        const course_id = "invalid";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the request body does not contain a `session_id` property", async () => {
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `session_id` body property is not uuid format", async () => {
+        const session_id = "invalid";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the request body does not contain a `totalModulesStudied` property", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `totalModulesStudied` body property is not a number", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: "not a number",
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the request body does not contain a `averageScore` property", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `averageScore` body property is not a number", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: "not a number",
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the request body does not contain a `averageScore` property", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+
+      it("the `averageScore` body property is not a number", async () => {
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: "not a number"
+          })
+        });
+
+        expect(res.status).to.equal(400);
+      });
+    });
+
+    describe("should return status 500 when", () => {
+      it("querying database fails", async () => {
+        // Mock database response
+        sandbox.stub(dbUtils, 'sql').throws();
+
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(500);
+      });
+    });
+
+    describe("should return status 201 when", () => {
+      it("successful request", async () => {
+        const user_id = "e014e4fd-db94-440a-b437-8f8eed897c20";
+        const course_id = "460ec7f6-eed6-41fa-ba9c-7ae80b239484";
+        const session_id = "c425ad42-4f13-4d98-a3c5-f2ad8efe32ed";
+
+        // Mock database response
+        sandbox.stub(dbUtils, 'sql').resolves();
+
+        const res = await fetch(`${BASE_URL}/courses/${course_id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-User-Id": user_id,
+          },
+          body: JSON.stringify({
+            sessionId: session_id,
+            totalModulesStudied: 0,
+            averageScore: 0,
+            timeStudied: 0
+          })
+        });
+
+        expect(res.status).to.equal(201);
+      });
+    });
+  });
+
 });

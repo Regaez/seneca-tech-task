eaabc868f8c42ef597670f6993d50e18f6f6ff6c
Thomas Threadgold <tj.threadgold@gmail.com>
2021-11-07 12:31:49 +0100

    fix: remove unnecessary sql config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,17 +1,6 @@
-import postgres from "postgres";
-
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "postgres";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
 export const DB_DATABASE = process.env.DB_DATABASE || "seneca";
 export const DB_PORT = parseInt(process.env.DB_PORT || "5432", 10);
 export const DB_CONNECTION_LIMIT = parseInt(process.env.DB_CONNECTION_LIMIT || "22", 10);
-
-export const sql = postgres({
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  username: DB_USER,
-  port: DB_PORT,
-  database: DB_DATABASE,
-  max: DB_CONNECTION_LIMIT
-});
